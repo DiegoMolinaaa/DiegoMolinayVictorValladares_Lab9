8da7fce2af5553f94de9b949a6963424d29aa04f
simplyvei <98425827+simplyvei@users.noreply.github.com>
2022-03-18 16:18:50 -0600

    prueba

--- src/diegomolinayvictorvalladares_lab9/login.java
+++ src/diegomolinayvictorvalladares_lab9/login.java
@@ -56,8 +56,8 @@ public class login extends javax.swing.JFrame {
         jLabel12 = new javax.swing.JLabel();
         jLabel13 = new javax.swing.JLabel();
         jLabel14 = new javax.swing.JLabel();
-        tf_nombre_alumno = new javax.swing.JTextField();
-        bt_registrarse_alumno = new javax.swing.JButton();
+        tf_nombre_cliente = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
         tf_user_alumno = new javax.swing.JTextField();
         tf_password_alumno = new javax.swing.JTextField();
         pf_password_alumno = new javax.swing.JPasswordField();
@@ -113,15 +113,9 @@ public class login extends javax.swing.JFrame {
         jPanel2.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 60, -1, -1));
 
         bt_registrarse_maestro.setText("Registrarse");
-<<<<<<< HEAD
-        bt_registrarse_maestro.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bt_registrarse_maestroActionPerformed(evt);
-=======
         bt_registrarse_maestro.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 bt_registrarse_maestroMouseClicked(evt);
->>>>>>> 2c498a9f021ffb9bc89d90eaf580740ea3320721
             }
         });
         jPanel2.add(bt_registrarse_maestro, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 260, -1, -1));
@@ -158,20 +152,15 @@ public class login extends javax.swing.JFrame {
         jLabel14.setForeground(new java.awt.Color(0, 0, 0));
         jLabel14.setText("Nombre");
         jPanel3.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 60, -1, -1));
-        jPanel3.add(tf_nombre_alumno, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 60, 180, -1));
+        jPanel3.add(tf_nombre_cliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 60, 180, -1));
 
-        bt_registrarse_alumno.setText("Registrarse");
-        bt_registrarse_alumno.addMouseListener(new java.awt.event.MouseAdapter() {
+        jButton1.setText("Registrarse");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                bt_registrarse_alumnoMouseClicked(evt);
+                jButton1MouseClicked(evt);
             }
         });
-        bt_registrarse_alumno.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bt_registrarse_alumnoActionPerformed(evt);
-            }
-        });
-        jPanel3.add(bt_registrarse_alumno, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 230, 100, 40));
+        jPanel3.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 230, 100, 40));
         jPanel3.add(tf_user_alumno, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 100, 180, -1));
         jPanel3.add(tf_password_alumno, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 140, 180, -1));
         jPanel3.add(pf_password_alumno, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 180, 180, -1));
@@ -448,10 +437,10 @@ public class login extends javax.swing.JFrame {
     private javax.swing.JButton bt_crudClase;
     private javax.swing.JButton bt_crudExamen;
     private javax.swing.JButton bt_crudPregunta;
-    private javax.swing.JButton bt_registrarse_alumno;
     private javax.swing.JButton bt_registrarse_maestro;
     private javax.swing.JButton bt_registro_m;
     private javax.swing.JButton bt_verClases;
+    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
@@ -480,7 +469,7 @@ public class login extends javax.swing.JFrame {
     private javax.swing.JFrame plataforma_maestro;
     private javax.swing.JFrame registro_alumno;
     private javax.swing.JFrame registro_maestro;
-    private javax.swing.JTextField tf_nombre_alumno;
+    private javax.swing.JTextField tf_nombre_cliente;
     private javax.swing.JTextField tf_nombre_maestro;
     private javax.swing.JTextField tf_password;
     private javax.swing.JTextField tf_password_alumno;
